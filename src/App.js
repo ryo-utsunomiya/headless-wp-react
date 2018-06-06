3791759873f965a80a6fba8dd7829b18f89898ea
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-06-07 00:21:03 +0900

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
+// import logo from './logo.svg';
 import './App.css';
 
+async function getPosts() {
+  // const url = 'https://www.ikyu.com/concierge/wp-json/ikyu/v1/posts?aid=00001290&type=closeup';
+  const url = 'https://www.ikyu.com/concierge/wp-json/wp/v2/posts';
+  const res = await axios.get(url).catch(console.error);
+  return res.data;
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      posts: await getPosts(),
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { posts } = this.state;
+    if (posts[0]) {
+      return (
+        <div dangerouslySetInnerHTML={{ __html: posts[0].content.rendered }}/>
+      );
+    }
+    return <div>No post</div>;
   }
 }
 
